319e78a36df62ed51af798136b317b04ab1dc455
juanjo00sg <jujo00sg@gmail.com>
2022-12-09 01:23:28 -0500

    Implementacion de guardianes segun tipo de usuario

---
+++ src/app/guardianes/operador.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SeguridadService } from '../services/seguridad.service';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OperadorGuard implements CanActivate {
+  constructor(private miServicioSeguridad: SeguridadService, private router: Router) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (this.miServicioSeguridad.sesionExiste() && this.miServicioSeguridad.VerificarRolSesion(environment.ID_ROL_AIRLINE_OPERATOR)) {
+        return true;
+      }
+      this.router.navigate(['pages/seguridad/login']);
+      return false;
+  }
+  
+}
---
+++ src/app/guardianes/usuario.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SeguridadService } from '../services/seguridad.service';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioGuard implements CanActivate {
+  constructor(private miServicioSeguridad: SeguridadService, private router: Router) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.miServicioSeguridad.sesionExiste() && this.miServicioSeguridad.VerificarRolSesion(environment.ID_ROL_USER)) {
+      return true;
+    }
+    this.router.navigate(['pages/seguridad/login']);
+    return false;
+  }
+}
+  
+
